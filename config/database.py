6f0f26f160e7faeb438d85a1eae96746792cad18
st-longngo <long.ngo@supremetech.vn>
2023-11-29 17:04:58 +0700

    Fix deploy cetifi

--- config/database.py
+++ config/database.py
@@ -1,6 +1,9 @@
 from pymongo import MongoClient
+import certifi
 
-client = MongoClient('mongodb+srv://admin:12345678aA@flashcard.ubuimgf.mongodb.net/?retryWrites=true&w=majority')
+ca = certifi.where()
+
+client = MongoClient('mongodb+srv://admin:12345678aA@flashcard.ubuimgf.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
 
 db = client['flashcard']
 
