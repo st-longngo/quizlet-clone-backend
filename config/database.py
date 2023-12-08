8928e4d290fb9520de743832196a1c2291a965f6
st-longngo <long.ngo@supremetech.vn>
2023-12-08 08:48:33 +0700

    update url of db

--- config/database.py
+++ config/database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://admin:12345678aA@flashcard.ubuimgf.mongodb.net/?ssl=true&ssl_cert_reqs=CERT_NONE&retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://CECS491:JMTXd9SH0JqUAgNb@studease.gfzetdu.mongodb.net/?ssl=true&ssl_cert_reqs=CERT_NONE&retryWrites=true&w=majority')
 
 db = client['flashcard']
 
